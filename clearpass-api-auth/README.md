2f50245a64b0bf013d5ad216d6c79a0c1ffbab89
Tim Cappalli <timc@hpe.com>
2017-12-26 16:26:05 -0500

    2017.03
    Fix path to params.cfg
    Added docstrings
    Removed printing of the payload
    API privileges and role now available

--- clearpass-api-auth/README.md
+++ clearpass-api-auth/README.md
@@ -1,20 +1,25 @@
 
 # clearpass-api-auth
 
-![version 2017.02](https://img.shields.io/badge/Version-2017.02-brightgreen.svg "version 2017.02") [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) ![Aruba Security Group](https://img.shields.io/badge/Source-Aruba_Security-orange.svg "Aruba Security Group")
+![version 2017.03](https://img.shields.io/badge/Version-2017.03-brightgreen.svg "version 2017.03") [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) ![Aruba Security Group](https://img.shields.io/badge/Source-Aruba_Security-orange.svg "Aruba Security Group")
 
 
 ## Overview
 This is a Python3 code snippet for obtaining an OAuth 2.0 access token for accessing the ClearPass REST API.
 
 ## Current Version
-2017.02 (2017-10-07)
+2017.03 (2017-12-26)
 
 ## Change Log
+### 2017.03 (2017-12-26)
+* Added docstrings to functions
+* Script no longer directly returns result, call the function with desired variable, ex: clearpassauth.access_token
+* Access role and privilieges are now pulled for the session, use: clearpassauth.api_privs or clearpassauth.api_role
+* Fix path to params.cfg
 ### 2017.02 (2017-10-07)
-Parsed out access_token, token_type, token_expires_in, and scope
+* Parsed out access_token, token_type, token_expires_in, and scope
 ### 2017.01 (2017-07-28)
-Initial Release
+* Initial Release
 
 ## Pre-requisites
 * An API client must be defined in ClearPass Guest under Administration » API Services » API Clients
@@ -27,9 +32,12 @@ Configure the required parameters in config/params.cfg:
 
 The remaining parameters vary by grant type.
 
-Call the script with no arguments: 
+Import the script as a function
 
-> `python3 clearpassauth.py`
+> `import clearpassauth`
+
+> `token = clearpassauth.access_token`
+> `my_privs = clearpassauth.api_privs`
 
 ## License
 Copyright (c) Hewlett Packard Enterprise Development LP. All Rights Reserved.
--- clearpass-api-auth/clearpassauth.py
+++ clearpass-api-auth/clearpassauth.py
@@ -4,7 +4,7 @@
 # Author: @timcappalli, Aruba Security Group
 # Organization: Aruba, a Hewlett Packard Enterprise company
 #
-# Version: 2017.02
+# Version: 2017.03
 #
 #
 # Copyright (c) Hewlett Packard Enterprise Development LP
@@ -28,10 +28,12 @@
 import requests
 import json
 from configparser import ConfigParser
+import os
 
 # configuration file parameters
+params = os.path.join(os.path.dirname(__file__), "config/params.cfg")
 config = ConfigParser()
-config.read('config/params.cfg')
+config.read(params)
 
 clearpass_fqdn = config.get('ClearPass', 'clearpass_fqdn')
 oauth_grant_type = config.get('OAuth2', 'grant_type')
@@ -43,6 +45,8 @@ oauth_password = config.get('OAuth2', 'password')
 
 # validate config
 def check_config(clearpass_fqdn, oauth_grant_type, oauth_client_id, oauth_client_secret, oauth_username, oauth_password):
+    """Validate the OAuth 2.0 configuration from the params.cfg file."""
+
     if not clearpass_fqdn:
         print('Error: ClearPass FQDN must be defined in config file (config/params.cfg)')
         exit(1)
@@ -58,6 +62,8 @@ def check_config(clearpass_fqdn, oauth_grant_type, oauth_client_id, oauth_client
 
 
 def get_access_token(clearpass_fqdn, oauth_grant_type, oauth_client_id, oauth_client_secret, oauth_username, oauth_password):
+    """Get OAuth 2.0 access token with config from params.cfg"""
+
     url = "https://" + clearpass_fqdn + "/api/oauth"
 
     headers = {'Content-Type':'application/json'}
@@ -73,9 +79,9 @@ def get_access_token(clearpass_fqdn, oauth_grant_type, oauth_client_id, oauth_cl
             print(e)
             exit(1)
 
-        json_response = json.loads(r.content)
+        json_response = json.loads(r.text)
 
-        return json.dumps(json_response)
+        return json_response
 
     # grant_type: password   public client
     if oauth_grant_type == "password" and not oauth_client_secret:
@@ -88,13 +94,13 @@ def get_access_token(clearpass_fqdn, oauth_grant_type, oauth_client_id, oauth_cl
             print(e)
             exit(1)
 
-        json_response = json.loads(r.content)
+        json_response = json.loads(r.text)
 
-        return json.dumps(json_response)
+        return json_response
 
     # grant_type: client_credentials
     if oauth_grant_type == "client_credentials":
-        payload = {'grant_type':oauth_grant_type, 'client_id': oauth_client_id, 'client_secret': oauth_client_secret}
+        payload = {'grant_type': oauth_grant_type, 'client_id': oauth_client_id, 'client_secret': oauth_client_secret}
 
         try:
             r = requests.post(url, headers=headers, json=payload)
@@ -103,18 +109,58 @@ def get_access_token(clearpass_fqdn, oauth_grant_type, oauth_client_id, oauth_cl
             print(e)
             exit(1)
 
-        json_response = json.loads(r.content)
+        json_response = json.loads(r.text)
 
-        return json.dumps(json_response)
+        return json_response
+
+def get_api_role(clearpass_fqdn, token_type, access_token):
+    """Get the current ClearPass operator profile name"""
+
+    url = "https://" + clearpass_fqdn + "/api/oauth/me"
+
+    headers = {'Content-Type':'application/json', "Authorization": "{} {}".format(token_type, access_token)}
+
+    try:
+        r = requests.get(url, headers=headers)
+        r.raise_for_status()
+    except Exception as e:
+        print(e)
+        exit(1)
+
+    json_response = json.loads(r.text)
+
+    return json_response
+
+
+def get_privs(clearpass_fqdn, token_type, access_token):
+    """Get the current access privileges"""
+
+    url = "https://" + clearpass_fqdn + "/api/oauth/privileges"
+
+    headers = {'Content-Type':'application/json', "Authorization": "{} {}".format(token_type, access_token)}
+
+    try:
+        r = requests.get(url, headers=headers)
+        r.raise_for_status()
+    except Exception as e:
+        print(e)
+        exit(1)
+
+    json_response = json.loads(r.text)
+
+    return json_response
 
 
 check_config(clearpass_fqdn, oauth_grant_type, oauth_client_id, oauth_client_secret, oauth_username, oauth_password)
 
-clearpass_response = get_access_token(clearpass_fqdn, oauth_grant_type, oauth_client_id, oauth_client_secret, oauth_username, oauth_password)
+token_response = get_access_token(clearpass_fqdn, oauth_grant_type, oauth_client_id, oauth_client_secret, oauth_username, oauth_password)
+access_token = token_response['access_token']
+token_type = token_response['token_type']
+token_expires_in = token_response['expires_in']
+scope = token_response['scope']
 
-access_token = clearpass_response['access_token']
-token_type = clearpass_response['token_type']
-token_expires_in = clearpass_response['expires_in']
-scope = clearpass_response['scope']
+get_api_role_response = get_api_role(clearpass_fqdn, token_type, access_token)
+api_role = get_api_role_response['info']
 
-print(clearpass_response)
+get_privs_response = get_privs(clearpass_fqdn, token_type, access_token)
+api_privs = get_privs_response['privileges']
